7aa5c7526920145cd41fbba7b3e95d4dd14708e3
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-03-02 21:09:04 +0100

    Add sample file to data lake init container

--- docker/images/datalake-init/entrypoint.sh
+++ docker/images/datalake-init/entrypoint.sh
@@ -1,5 +1,9 @@
+# Create the data lake storage containers
 az storage container create --name flows
 az storage container create --name bronze
 az storage container create --name silver
 az storage container create --name gold
 az storage container create --name sandbox
+
+# Upload some sample data to the datalake.
+az storage blob upload --container-name gold --name iris/2023/03/02/iris.csv --file /opt/datalake-init/data/iris.csv --overwrite
