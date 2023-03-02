bf7fdd4844eae8455f79cf67dc659fdd4ecda14b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-03-02 22:08:04 +0100

    Fix connection string property

--- docker/images/workflow-server-init/deploy_blocks.py
+++ docker/images/workflow-server-init/deploy_blocks.py
@@ -6,6 +6,7 @@ so we don't have to manually configure them using the Prefect UI.
 import asyncio
 from os import getenv
 
+import dotenv
 from prefect.filesystems import Azure
 
 
@@ -17,18 +18,22 @@ async def deploy_azure_flows_block():
     The flows container is used to store prefect flow deployments.
     """
 
+    dotenv.load_dotenv()
+
     connection_string = getenv("AZURE_CONNECTION_STRING", "")
 
     if connection_string == "":
         print("AZURE_CONNECTION_STRING environment variable not set.")
         exit(1)
+    else:
+        print(connection_string)
 
     print("Deploying Azure flows block to Prefect...")
 
     storage = Azure(
         name="flows",
         bucket_path="flows",
-        connection_string=connection_string
+        azure_storage_connection_string=connection_string,
     )
 
     await storage.save("flows", overwrite=True)
