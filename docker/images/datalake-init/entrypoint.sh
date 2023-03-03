e294588ca313b6ca4fa28569fb16246870595692
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-03-03 07:41:11 +0100

    Rename containers

--- docker/images/datalake-init/entrypoint.sh
+++ docker/images/datalake-init/entrypoint.sh
@@ -1,9 +1,8 @@
 # Create the data lake storage containers
 az storage container create --name flows
-az storage container create --name bronze
-az storage container create --name silver
-az storage container create --name gold
-az storage container create --name sandbox
+az storage container create --name raw
+az storage container create --name intermediate
+az storage container create --name preprocessed
 
 # Upload some sample data to the datalake.
-az storage blob upload --container-name gold --name iris/2023/03/02/iris.csv --file /opt/datalake-init/data/iris.csv --overwrite
+az storage blob upload --container-name raw --name iris/2023/03/02/iris.csv --file /opt/datalake-init/data/iris.csv --overwrite
