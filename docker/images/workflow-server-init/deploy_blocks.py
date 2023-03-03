1e06d0bd36bec4a12dc0aa6b4458d45073e8ef0c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-03-03 07:41:21 +0100

    Deploy raw storage block

--- docker/images/workflow-server-init/deploy_blocks.py
+++ docker/images/workflow-server-init/deploy_blocks.py
@@ -10,7 +10,7 @@ import dotenv
 from prefect.filesystems import Azure
 
 
-async def deploy_azure_flows_block():
+async def deploy_azure_block(name: str, path: str) -> None:
     """
     Deploys an Azure block to the Prefect environment that points to the `flows`
     container in the Azure Storage Account Emulator.
@@ -26,20 +26,20 @@ async def deploy_azure_flows_block():
         print("AZURE_CONNECTION_STRING environment variable not set.")
         exit(1)
 
-    print("Deploying Azure flows block to Prefect...")
+    print(f"Deploying Azure block '{name}' to Prefect...")
 
     storage = Azure(
-        name="flows",
-        bucket_path="flows",
+        name=name,
+        bucket_path=path,
         azure_storage_connection_string=connection_string,
     )
 
-    await storage.save("flows", overwrite=True)
+    await storage.save(name, overwrite=True)
 
 
 async def main():
-    await deploy_azure_flows_block()
-
+    await deploy_azure_block("flows", "flows")
+    await deploy_azure_block("raw", "raw")
 
 if __name__ == "__main__":
     asyncio.run(main())
